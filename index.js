126db1233872d780a3456de6188cf34da0178268
Georgeches <georgechesire83@gmail.com>
2023-03-16 15:15:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main").remove()
+var newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'George is the champion'
+document.body.append(newHeader)
